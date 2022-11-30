3825748c8fcfaa5ae0074ca5cb3f3ef21bd7f901
qp2194 <qp2194@gmail.com>
2022-11-29 23:00:48 -0600

    boilerplate

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
-
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import { Cart, ProductDetail, Homepage, Products } from './pages';
 
 function App() {
   return (
-    <div className="App">
-     App
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path='/' element={<Homepage />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/product/:id' element={<ProductDetail />}/>
+        <Route path='/cart' element={<Cart />}/>
+        {/* <Route path='/success'>
+          Checkout Success
+        </Route> */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,4 @@
+export {default as Cart} from './Cart/Cart';
+export {default as Homepage} from './Home/Homepage';
+export {default as Products} from './Products/Products';
+export {default as ProductDetail} from './ProductDetail/ProductDetail';
