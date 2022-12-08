dfed65ddcf28a834622e236e26ec77242f5c2282
qp2194 <qp2194@gmail.com>
2022-12-07 23:39:31 -0600

    add mobile nav

--- frontend/src/components/Carousel/Carousel.jsx
+++ frontend/src/components/Carousel/Carousel.jsx
@@ -1,40 +1,50 @@
 import React from "react";
 import Slider from "react-slick";
-import img01 from '../../images/imac.jpeg';
-import img02 from '../../images/ipad-pro.jpeg';
-import img03 from '../../images/ipad.jpeg';
-import img04 from '../../images/iphone-14-pro.jpeg';
-import './Carousel.css';
-import "slick-carousel/slick/slick.css"; 
+import "./Carousel.css";
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const Carousel = () => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        slidesToShow: 3,
-        speed: 500,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        
-      };
+  const array = [
+    {
+      name: "slide 1",
+      background: "black",
+    },
+    {
+      name: "slide 2",
+      background: "white",
+    },
+    {
+      name: "slide 3",
+      background: "red",
+    },
+    {
+      name: "slide 4",
+      background: "blue",
+    },
+  ];
+  const settings = {
+    dots: true,
+    infinite: true,
+    slidesToShow: 3,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  };
   return (
     <div className="carousel">
-     <Slider {...settings}>
-          <div>
-            <img src={img01} alt="img-slider"/>
-          </div>
-          <div>
-            <img src={img02} alt="img-slider"/>
-          </div>
-          <div>
-            <img src={img03} alt="img-slider"/>
-          </div>
-          <div>
-            <img src={img04} alt="img-slider"/>
-          </div>
-        </Slider>
-  </div>
+      <Slider {...settings}>
+        {array.map((item, index) => {
+          return (
+            <div key={index} className="slider-div">
+              <div style={{background: item.background,minHeight:'400px'}}>
+                {item.name}
+              </div>
+            </div>
+          );
+        })}
+      </Slider>
+    </div>
   );
 };
 
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,36 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
 import AppleIcon from "@mui/icons-material/Apple";
-import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import ShoppingBagIcon from "@mui/icons-material/ShoppingBag";
 // import SearchIcon from '@mui/icons-material/Search';
 
 const Navbar = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  const handleHamburger = () => {
+    setIsNavOpen(!isNavOpen);
+  };
   return (
     <div className="navbar">
       <Container maxWidth="md">
         <Box className="nav-container">
-          <Link to="/">
-            <AppleIcon />
-          </Link>
-          <ul>
-            <li>
-              <Link to="/products">Store</Link>
-            </li>
-            <li>
-              <Link to="/">Mac</Link>
-            </li>
-            <li>
-              <Link to="/">iPad</Link>
-            </li>
-            <li>
-              <Link to="/">iPhone</Link>
-            </li>
-          </ul>
-          <div className="cart">
-            <ShoppingCartIcon />
+          <div
+            class={`hamburger ${isNavOpen ? "open" : ""}`}
+            onClick={handleHamburger}
+          >
+            <div class="line line-1">
+              <div class="line-inner line-inner-1"></div>
+            </div>
+            <div class="line line-2">
+              <div class="line-inner line-inner-2"></div>
+            </div>
+          </div>
+          <div className="nav-icons">
+            <Link to="/">
+              <AppleIcon />
+            </Link>
+          </div>
+          <div className={`nav-list ${isNavOpen ? "active" : ""}`}>
+            <ul>
+              <li>
+                <Link to="/products">Store</Link>
+              </li>
+              <li>
+                <Link to="/products/mac">Mac</Link>
+              </li>
+              <li>
+                <Link to="/products/ipad">iPad</Link>
+              </li>
+              <li>
+                <Link to="/products/iphone">iPhone</Link>
+              </li>
+            </ul>
+          </div>
+          <div className="cart nav-icons">
+            <ShoppingBagIcon />
           </div>
         </Box>
       </Container>
