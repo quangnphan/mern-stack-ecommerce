28f487d105fdc40868a55fe16e9bbec81fd446c1
Loc Nguyen <locnguyen91194@gmail.com>
2023-01-31 03:27:15 -0600

    feat: add Product to cart

--- frontend/src/app/slices/cartSlice.js
+++ frontend/src/app/slices/cartSlice.js
@@ -5,30 +5,35 @@ const cartSlice = createSlice({
   initialState: {
     products: [
       {
-        id: 1,
+        // id: 1,
         name: "iPhone",
         storage: "128gb",
         color: "Space Gray",
-        price: "1099",
-        quantity: "1",
+        price: 1099,
+        quantity: 1,
       },
       {
-        id: 2,
+        // id: 2,
         name: "Macbook",
         storage: "128gb",
         color: "Space Gray",
-        price: "2099",
-        quantity: "1",
+        price: 2099,
+        quantity: 1,
       },
     ],
     quantity: 2,
-    total: 4000,
+    subTotal: 3198,
+    tax: 0,
+    shipping: 7,
+    total: 0,
   },
   reducers: {
     addProduct: (state, action) => {
       state.quantity += 1;
       state.products.push(action.payload);
-      state.total += action.payload.price * action.payload.quantity;
+      state.subTotal += action.payload.price * action.payload.quantity;
+      state.tax = (state.subTotal*8.25/100);
+      state.total = (state.subTotal + state.tax + state.shipping).toFixed(2);
     },
   },
 });
