a8588ca0dd4a38b68b53b29d48bc391747d6d0ae
qp2194 <qp2194@gmail.com>
2023-02-24 23:23:56 -0600

    fix: route

--- frontend/src/helper.js
+++ frontend/src/helper.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = `http://localhost:8000/api/ecom`;
+const baseURL = `http://mernstack-ecommerce-03q21.netlify.app/api/ecom`;
 
 export default axios.create({
     baseURL: baseURL,
