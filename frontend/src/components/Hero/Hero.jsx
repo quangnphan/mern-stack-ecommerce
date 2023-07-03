0f272a3028e076df9826ae1c21afbc82517e59fc
qp2194 <qp2194@gmail.com>
2023-07-03 17:41:00 -0500

    fix style

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Container,Typography} from "@mui/material";
 import { Link } from 'react-router-dom';
 import './Hero.css';
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 const Hero = () => {
   return (
@@ -15,7 +16,7 @@ const Hero = () => {
             </Typography>
             <Typography variant="h5">
             <Link to="/product/64a20676c0b0ab3f55eba0b5">
-                Learn more
+                Learn more <span><ArrowForwardIosIcon /></span>
             </Link>
             </Typography>
         </Container>
